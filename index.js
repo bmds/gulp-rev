c50b3b64e95ae258625a68371183f356f89b24df
Barney Scott <barney.scott@codecomputerlove.com>
2017-07-03 13:05:44 +0100

    Add manifestObj and manifestMerge for rough object based manifest

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const revPath = require('rev-path');
 const sortKeys = require('sort-keys');
 const modifyFilename = require('modify-filename');
 
+let manifestObject = {};
+
 function relPath(base, filePath) {
 	if (filePath.indexOf(base) !== 0) {
 		return filePath.replace(/\\/g, '/');
@@ -46,6 +48,25 @@ const getManifestFile = opts => vinylFile.read(opts.path, opts).catch(err => {
 
 	throw err;
 });
+
+function addStreamFileToManifest(file, manifest) {
+	const revisionedFile = relPath(file.base, file.path);
+	const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
+
+	manifest[originalFile] = revisionedFile;
+}
+
+function mergeManifest(manifestFile, manifest, transformer) {
+	let oldManifest = {};
+
+	try {
+		oldManifest = transformer.parse(manifestFile.contents.toString());
+	} catch (err) {}
+
+	return Object.assign(oldManifest, manifest);
+}
+
+const isRevedFile = file => file.path && file.revOrigPath;
 
 const plugin = () => {
 	const sourcemaps = [];
@@ -125,10 +146,7 @@ plugin.manifest = (pth, opts) => {
 			return;
 		}
 
-		const revisionedFile = relPath(file.base, file.path);
-		const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
-
-		manifest[originalFile] = revisionedFile;
+		addStreamFileToManifest(file, manifest);
 
 		cb();
 	}, function (cb) {
@@ -140,19 +158,40 @@ plugin.manifest = (pth, opts) => {
 
 		getManifestFile(opts).then(manifestFile => {
 			if (opts.merge && !manifestFile.isNull()) {
-				let oldManifest = {};
-
-				try {
-					oldManifest = opts.transformer.parse(manifestFile.contents.toString());
-				} catch (err) {}
-
-				manifest = Object.assign(oldManifest, manifest);
+				manifest = mergeManifest(manifestFile, manifest, opts.transformer);
 			}
 
 			manifestFile.contents = Buffer.from(opts.transformer.stringify(sortKeys(manifest), null, '  '));
 			this.push(manifestFile);
 			cb();
 		}).catch(cb);
+	});
+};
+
+plugin.manifestObj = () => {
+	return through.obj((file, enc, cb) => {
+		// ignore all non-rev'd files
+		if (!isRevedFile(file)) {
+			cb();
+			return;
+		}
+
+		addStreamFileToManifest(file, manifestObject);
+		cb();
+	});
+};
+
+plugin.manifestMerge = function (opts) {
+	opts = Object.assign({
+		transformer: JSON
+	}, opts);
+
+	return through.obj(function (file, enc, cb) {
+		manifestObject = mergeManifest(file, manifestObject, opts.transformer);
+
+		file.contents = Buffer.from(opts.transformer.stringify(sortKeys(manifestObject), null, '  '));
+		this.push(file);
+		cb();
 	});
 };
 
